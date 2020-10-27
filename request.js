b5fff93877bee72dcc79661528f4e479bfbfcda2
Yashas12 <48153404+Yashas12@users.noreply.github.com>
2020-10-27 09:35:19 +0530

    Update request.js

--- request.js
+++ request.js
@@ -1,9 +1,9 @@
 const key = '{api-key}';
 
 /*To generate an API key, go to Openweatherapi.org and create an account.
-After creating an account, you will be asked to verify your email(that you have used to create the account).
-After verifying, you'll recieve an email containing your API key. Replace {api-key} in line 1 with your API key and your weather 
-applicaton is ready to go.
+After creating an account, you will recieve an email asking you to verify your email.
+After verifying, you'll recieve another email containing your API key. Replace {api-key} in line 1 with your API key.
+Lo and behold! Your weather applicaton is ready to go.
     */
 
 /*const baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=Lagos&appid={api-key}'
