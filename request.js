5cd3c71f3de179bf25c43cdca493ac3fc2e5dfff
Yashas12 <48153404+Yashas12@users.noreply.github.com>
2020-10-27 09:46:19 +0530

    Update request.js

--- request.js
+++ request.js
@@ -1,6 +1,6 @@
 const key = '{api-key}';
 
-/*To generate an API key, go to Openweatherapi.org and create an account.
+/*To generate an API key, go to Openweatherapimap.org and create an account.
 After creating an account, you will recieve an email asking you to verify your email.
 After verifying, you'll recieve another email containing your API key. Replace {api-key} in line 1 with your API key.
 Lo and behold! Your weather applicaton is ready to go.
